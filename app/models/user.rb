480599ae739827d7bfad0b87cb87709a9e63151a
Miles Starkenburg <milesstarkenburg@gmail.com>
2013-10-05 22:01:07 -0700

    List User's Posts with their show pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
+  has_many :posts
 
   validates :name, presence: true, length: {minimum: 3} 
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i}
